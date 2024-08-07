be9939fee0e2a1fdc504c57ed6ce4ed8ceaa70f4
teyfikaktas <aktasteyf@gmail.com>
2024-08-08 01:27:42 +0300

    logo add

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -2,18 +2,31 @@
     class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark"
     id="sidenav-main">
     <div class="sidenav-header">
-        <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-            aria-hidden="true" id="iconSidenav"></i>
-            <a class="navbar-brand m-0 d-flex text-wrap align-items-center" href=" {{ route('dashboard') }} ">
-                <img src="{{ asset('assets') }}/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-2 font-weight-bold text-white">Material Dashboard 2 Laravel Livewire</span>
-            </a>
-    </div>
+    <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+        aria-hidden="true" id="iconSidenav"></i>
+    <a class="navbar-brand m-0 d-flex text-wrap align-items-center" href="{{ route('dashboard') }}">
+        <!-- Eğitimle ilgili bir şey burada yer alacak -->
+        <svg width="40" height="40" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" class="navbar-brand-img h-100">
+            <rect x="12" y="8" width="40" height="48" rx="2" ry="2" fill="white" stroke="black" stroke-width="2"/>
+            <line x1="12" y1="16" x2="52" y2="16" stroke="black" stroke-width="2"/>
+            <line x1="12" y1="24" x2="52" y2="24" stroke="black" stroke-width="2"/>
+            <line x1="12" y1="32" x2="52" y2="32" stroke="black" stroke-width="2"/>
+            <line x1="12" y1="40" x2="52" y2="40" stroke="black" stroke-width="2"/>
+            <line x1="12" y1="48" x2="52" y2="48" stroke="black" stroke-width="2"/>
+            <line x1="16" y1="8" x2="16" y2="56" stroke="black" stroke-width="2"/>
+            <line x1="48" y1="8" x2="48" y2="56" stroke="black" stroke-width="2"/>
+            <line x1="8" y1="56" x2="56" y2="56" stroke="black" stroke-width="2"/>
+            <path d="M24 8 L32 2 L40 8" fill="none" stroke="black" stroke-width="2"/>
+        </svg>
+        <span class="ms-2 font-weight-bold text-white">Eğitim Yönetim Sistemi</span>
+    </a>
+</div>
+
     <hr class="horizontal light mt-0 mb-2">
     <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item mt-3">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Laravel examples</h6>
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Kullanıcı Yönetimi</h6>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white {{ Route::currentRouteName() == 'user-profile' ? ' active bg-gradient-primary' : '' }} "
