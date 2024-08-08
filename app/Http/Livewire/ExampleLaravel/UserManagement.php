d7b83a83aec940941b6b4f9ac645f736bedb4ca9
teyfikaktas <aktasteyf@gmail.com>
2024-08-08 03:13:10 +0300

    das

--- app/Http/Livewire/ExampleLaravel/UserManagement.php
+++ app/Http/Livewire/ExampleLaravel/UserManagement.php
@@ -12,6 +12,7 @@ class UserManagement extends Component
     use WithPagination;
 
     public $name, $email, $phone, $location, $about, $password;
+    public $teacher_name, $class_code, $is_teacher;
     public $isEditing = false;
     public $editingUserId;
 
@@ -21,6 +22,9 @@ class UserManagement extends Component
         'phone' => 'nullable',
         'location' => 'nullable',
         'about' => 'nullable',
+        'teacher_name' => 'nullable',
+        'class_code' => 'nullable',
+        'is_teacher' => 'boolean',
         'password' => 'required|min:6',
     ];
 
@@ -42,11 +46,14 @@ class UserManagement extends Component
             'phone' => $this->phone,
             'location' => $this->location,
             'about' => $this->about,
+            'teacher_name' => $this->teacher_name,
+            'class_code' => $this->class_code,
+            'is_teacher' => $this->is_teacher,
             'password' => Hash::make($this->password),
         ]);
 
         $this->resetForm();
-        session()->flash('message', 'User successfully created.');
+        session()->flash('message', 'Kullanıcı başarıyla oluşturuldu.');
     }
 
     public function edit($id)
@@ -59,6 +66,9 @@ class UserManagement extends Component
         $this->phone = $user->phone;
         $this->location = $user->location;
         $this->about = $user->about;
+        $this->teacher_name = $user->teacher_name;
+        $this->class_code = $user->class_code;
+        $this->is_teacher = $user->is_teacher;
     }
 
     public function update()
@@ -69,6 +79,9 @@ class UserManagement extends Component
             'phone' => 'nullable',
             'location' => 'nullable',
             'about' => 'nullable',
+            'teacher_name' => 'nullable',
+            'class_code' => 'nullable',
+            'is_teacher' => 'boolean',
         ]);
 
         $user = User::findOrFail($this->editingUserId);
@@ -78,10 +91,13 @@ class UserManagement extends Component
             'phone' => $this->phone,
             'location' => $this->location,
             'about' => $this->about,
+            'teacher_name' => $this->teacher_name,
+            'class_code' => $this->class_code,
+            'is_teacher' => $this->is_teacher,
         ]);
 
         $this->resetForm();
-        session()->flash('message', 'User successfully updated.');
+        session()->flash('message', 'Kullanıcı başarıyla güncellendi.');
     }
 
     public function resetForm()
@@ -91,6 +107,9 @@ class UserManagement extends Component
         $this->phone = '';
         $this->location = '';
         $this->about = '';
+        $this->teacher_name = '';
+        $this->class_code = '';
+        $this->is_teacher = false;
         $this->password = '';
         $this->isEditing = false;
         $this->editingUserId = null;
